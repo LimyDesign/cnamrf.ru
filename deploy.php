90ca62de22d6a273e0791c5bd3f0aad64f39e32f
Arsen Bespalov <me@arsen.pw>
2015-04-10 15:26:53 +0800

    update deploy script

--- deploy.php
+++ deploy.php
@@ -23,7 +23,7 @@ function run() {
 	}
 
 	// check if the request comes from github server
-	$github_ips = array('207.97.227.253', '50.57.128.197', '108.171.174.178', '50.57.231.61');
+	$github_ips = array('207.97.227.253', '50.57.128.197', '108.171.174.178', '50.57.231.61', '192.30.252.41');
 	if (in_array($_SERVER['REMOTE_ADDR'], $github_ips)) {
 		foreach ($config['endpoints'] as $endpoint) {
 			// check if the push came from the right repository and branch

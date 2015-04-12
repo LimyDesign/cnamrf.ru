58e45e4bba17d754fabdd184787efc5a2bf3eb74
Arsen Bespalov <me@arsen.pw>
2015-04-12 22:07:22 +0800

    update cabinet

--- cabinet/index.php
+++ cabinet/index.php
@@ -2,24 +2,24 @@
 session_start();
 
 // автозагрузчик классов
-spl_autoload_register(function ($class) {
-	$prefix = 'ld\\cnamrf\\';
-	$basedir = __DIR__ . '/src/';
-	$len = strlen($prefix);
+// spl_autoload_register(function ($class) {
+// 	$prefix = 'ld\\cnamrf\\';
+// 	$basedir = __DIR__ . '/src/';
+// 	$len = strlen($prefix);
 
-	if (strncmp($prefix, $class, $len) !== 0) {
-		return;
-	}
+// 	if (strncmp($prefix, $class, $len) !== 0) {
+// 		return;
+// 	}
 
-	$relative_class = substr($class, $len);
-	$file = $basedir . str_replace('\\', '/', $relative_class) . '.php';
+// 	$relative_class = substr($class, $len);
+// 	$file = $basedir . str_replace('\\', '/', $relative_class) . '.php';
 
-	if (file_exists($file)) {
-		require $file;
-	}
-});
+// 	if (file_exists($file)) {
+// 		require $file;
+// 	}
+// });
 
-$cabinet = new ld\cnamrf\cabinet;
+// $cabinet = new ld\cnamrf\cabinet;
 $loader = new Twig_Loader_Filesystem(__DIR__.'/tempaltes');
 $twig = new Twig_Environment($loader, array(
 	'cache' => __DIR__.'/cache',

875df6185ab7b48ba264200f938edf5c020ded42
Arsen Bespalov <me@arsen.pw>
2015-04-13 00:45:18 +0800

    update cabinet

--- cabinet/index.php
+++ cabinet/index.php
@@ -8,6 +8,7 @@ require_once __DIR__.'/src/vendor/autoload.php';
 $loader = new Twig_Loader_Filesystem(__DIR__.'/templates');
 $twig = new Twig_Environment($loader, array(
 	'cache' => __DIR__.'/cache',
+	'auto_reload' => true,
 ));
 
 if ($_SESSION['auth'] == 'true') 

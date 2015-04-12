35eca10142fe80f956d11ffb184e9914f2dc046d
Arsen Bespalov <me@arsen.pw>
2015-04-13 01:49:44 +0800

    update cabinet

--- cabinet/index.php
+++ cabinet/index.php
@@ -17,11 +17,19 @@ if ($_SESSION['auth'] == 'true')
 else
 {
 	echo $twig->render('auth.html', array(
-		'fb_link' => '#facebook',
-		'vk_link' => '#vkontakte',
-		'gp_link' => '#google-plus',
-		'ok_link' => '#odnoklassniki',
-		'mr_link' => '#mailru',
-		'ya_link' => '#yandex'));
+		'fb_link' => 'https://www.facebook.com/dialog/oauth?' . login_query('facebook'),
+		'vk_link' => 'https://oauth.vk.com/authorize?' . login_query('vkontakte'),
+		'gp_link' => 'https://accounts.google.com/o/oauth2/auth?' . login_query('google-plus'),
+		'ok_link' => 'http://www.odnoklassniki.ru/oauth/authorize?' . login_query('odnoklassniki'),
+		'mr_link' => 'https://connect.mail.ru/oauth/authorize?' . login_query('mailru'),
+		'ya_link' => 'https://oauth.yandex.ru/authorize?' . login_query('yandex')));
+}
+
+function login_query ($provider) {
+	if ($provider == 'facebook') {
+		return rawurlencode('client_id=' . $client_id[$provider] . 
+			'&scope=email&redirect_uri=http://' . $_SERVER['SERVER_NAME'] .
+			'response_type=code')
+	}
 }
 ?>

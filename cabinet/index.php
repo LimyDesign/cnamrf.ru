e1420a310b8b98a2149b8605a3016a82df3722ba
Arsen Bespalov <me@arsen.pw>
2015-04-12 21:48:56 +0800

    update cabinet

--- cabinet/index.php
+++ cabinet/index.php
@@ -1,7 +1,25 @@
 <?php
 session_start();
-use LD\cnamrf\Cabinet as Cabinet;
-$cabinet = new Cabinet;
+
+// автозагрузчик классов
+spl_autoload_register(function ($class) {
+	$prefix = 'ld\\cnamrf\\';
+	$basedir = __DIR__ . '/src/';
+	$len = strlen($prefix);
+
+	if (strncmp($prefix, $class, $len) !== 0) {
+		return;
+	}
+
+	$relative_class = substr($class, $len);
+	$file = $basedir . str_replace('\\', '/', $relative_class) . '.php';
+
+	if (file_exists($file)) {
+		require $file;
+	}
+});
+
+$cabinet = new ld\cnamrf\cabinet;
 
 if ($_SESSION['auth'] == 'true') 
 {

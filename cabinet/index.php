bdb16dc199fc903ac122525dd888bf0b593f8ac5
Arsen Bespalov <me@arsen.pw>
2015-04-12 22:38:49 +0800

    update cabinet

--- cabinet/index.php
+++ cabinet/index.php
@@ -5,7 +5,7 @@ session_start();
 // автозагрузчик классов
 require_once __DIR__.'/src/vendor/autoload.php';
 
-$loader = new Twig_Loader_Filesystem(__DIR__.'/tempaltes');
+$loader = new Twig_Loader_Filesystem(__DIR__.'/templates');
 $twig = new Twig_Environment($loader, array(
 	'cache' => __DIR__.'/cache',
 ));

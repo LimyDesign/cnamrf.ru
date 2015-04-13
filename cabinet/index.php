04a6f32d36243662c4ff3f0f43a8524e74db5f41
Arsen Bespalov <me@arsen.pw>
2015-04-13 11:12:38 +0800

    update cabinet

--- cabinet/index.php
+++ cabinet/index.php
@@ -11,6 +11,8 @@ $twig = new Twig_Environment($loader, array(
 	'auto_reload' => true,
 ));
 
+$conf = json_decode(file_get_contents(__DIR__.'/config.json'), true);
+
 if ($_SESSION['auth'] == 'true') 
 {
 }
@@ -26,9 +28,23 @@ else
 }
 
 function login_query ($provider) {
+	global $conf;
 	$redirect_uri = rawurlencode('http://'.$_SERVER['SERVER_NAME'].'/cabinet/auth/'.$provider);
+	$state = sha1($_SERVER['HTTP_USER_AGENT'].time());
+
 	if ($provider == 'facebook') {
-		return 'client_id='.$client_id[$provider].'&scope=email&redirect_uri='.$redirect_uri.'&response_type=code';
+		return 'client_id='.$conf->provider->facebook->CLIENT_ID.'&scope=email&redirect_uri='.$redirect_uri.'&response_type=code';
+	} elseif ($provider == 'vkontakte') {
+		return 'client_id='.$conf->provider->vkontakte->CLIENT_ID.'&scope=email&redirect_uri='.$redirect_uri.'&response_type=code&v=5.29&state='.$state.'&display=page';
+	} elseif ($provider == 'google-plus') {
+		$gp_scope = rawurlencode('https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile');
+		return 'client_id='.$conf->provider->googleplus->CLIENT_ID.'&scope='.$gp_scope.'&redirect_uri='.$redirect_uri.'&response_type=code&state='.$state.'&access_type=online&approval_prompt=auto&login_hint=email&include_granted_scopes=true';
+	} elseif ($provider == 'odnoklassniki') {
+		return 'client_id='.$conf->provider->odnoklassniki->CLIENT_ID.'&scope=GET_EMAIL&response_type=code&redirect_uri='.$redirect_uri.'&state='.$state;
+	} elseif ($provider == 'mailru') {
+		return 'client_id'.$conf->provider->mailru->CLIENT_ID.'&response_type=code&redirect_uri='.$redirect_uri;
+	} elseif ($provider == 'yandex') {
+		return 'client_id'.$conf->provider->yandex->CLIENT_ID.'&response_type=code&state='.$state;
 	}
 }
 ?>

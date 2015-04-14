5da9a0fe8ac105f47594ef7fdc2569b7c4c50360
Arsen Bespalov <me@arsen.pw>
2015-04-14 13:57:06 +0800

    update cabinet

--- cabinet/index.php
+++ cabinet/index.php
@@ -210,6 +210,7 @@ function auth ($provider) {
 		curl_setopt($curl, CURLOPT_URL, 'http://www.appsmail.ru/platform/api?'.$data);
 		curl_setopt($curl, CURLOPT_POST, false);
 		$res = json_decode(curl_exec($curl));
+		die($res);
 		auth_db($res->uid, $res->email, $provider);
 	} elseif ($provider == 'yandex') {
 		$data = http_build_query(array(

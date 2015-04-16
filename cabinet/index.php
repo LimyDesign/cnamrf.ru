7d8997cbd184f1d14decd2055b80eb8c2f130366
Arsen Bespalov <me@arsen.pw>
2015-04-16 12:14:58 +0800

    update cabinet

--- cabinet/index.php
+++ cabinet/index.php
@@ -337,7 +337,7 @@ function generateInvoice($summ) {
 	$html = file_get_contents(__DIR__.'/templates/invoice2.html');
 	$pdf->writeHTML($html, true, 0, true, 0);
 	$pdf->Image(K_PATH_IMAGES . 'print_trans.png', 21, 145, 40, '', '', '', '', false);
-	$pdf->Image(K_PATH_IMAGES . 'sign_trans.png', 60, 130, 60, '', '', '', '', false);
+	$pdf->Image(K_PATH_IMAGES . 'sign_trans.png', 60, 120, 60, '', '', '', '', false);
 	$pdf->lastPage();
 	$pdf->Output('invoice.pdf', 'D');
 }

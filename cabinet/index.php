0f1d33341c3dbda7d1b18eca0d6d0d1041137373
Arsen Bespalov <me@arsen.pw>
2015-04-19 00:28:06 +0800

    update cabinet

--- cabinet/index.php
+++ cabinet/index.php
@@ -364,7 +364,7 @@ function getUserBalans($return = false) {
 }
 
 function checkOrder() {
-	$post = implode($_POST);
+	$post = implode($_POST) . 'adasdas';
 	file_put_contents('tmp.txt', $post);
 }
 

753984ee73945d83820081abcfc77ff03d6d6a7b
Arsen Bespalov <me@arsen.pw>
2015-04-26 12:52:59 +0800

    update cabinet

---
+++ js/jquery.shakeit.js
@@ -0,0 +1,12 @@
+//ShakeIt Plugin 
+jQuery.fn.shakeit = function(intShakes, intDistance, intDuration) {
+    this.each(function() {
+        $(this).css("position","relative")
+        for (var x=1; x<=intShakes; x++) {
+        $(this).animate({left:(intDistance*-1)}, (((intDuration/intShakes)/4)))
+    .animate({left:intDistance}, ((intDuration/intShakes)/2))
+    .animate({left:0}, (((intDuration/intShakes)/4)))
+    }
+  })
+return this
+}

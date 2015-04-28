45c2e40bc361ec2670b66fdffa077459e57bcf22
Arsen Bespalov <me@arsen.pw>
2015-04-28 13:49:16 +0800

    update cabinet

--- cabinet/index.php
+++ cabinet/index.php
@@ -885,7 +885,6 @@ function yandexPayments($cmd) {
 		if ($conf['db']['type'] == 'postgres') {
 			$db = pg_connect('dbname='.$conf['db']['database']) or die('Невозможно подключиться к БД: '.pg_last_error());
 			$query = "select id, uid, invoice, sum from invoices where uid = {$yaCustomerNumber} and invoice = {$yaInvoiceId} and sum = {$yaOrderSumAmount}";
-			file_put_contents('aviso_1.log', $query);
 			$result = pg_query($query);
 			$iid = pg_fetch_result($result, 0, 'id');
 			$uid = pg_fetch_result($result, 0, 'uid');
@@ -905,6 +904,7 @@ function yandexPayments($cmd) {
 				$md5 = strtoupper(md5(implode(';', $checkOrderStr)));
 				if ($md5 != $yaMD5) {
 					$code = '1';
+					file_put_contents('aviso_1.log', $md5 . "\n" . $checkOrderStr . "\n");
 				} else {
 					$code = '0';
 					$query = "insert into log (uid, debet, client, invoice) values ({$uid}, {$sum}, '{$client}', {$iid}";

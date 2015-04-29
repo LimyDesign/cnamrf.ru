d63179d182e560188e7410a7ccda544b1193e125
Arsen Bespalov <me@arsen.pw>
2015-04-29 20:56:15 +0800

    update admin page

--- cabinet/index.php
+++ cabinet/index.php
@@ -885,8 +885,6 @@ function yandexPayments($cmd) {
 	{
 		if ($conf['db']['type'] == 'postgres') {
 			$db = pg_connect('dbname='.$conf['db']['database']) or die('Невозможно подключиться к БД: '.pg_last_error());
-			// if ($yaPaymentType != 'PC') 
-			// 	$yaInvoiceId = $yaInvoiceId - 1;
 			$query = "select id, uid, invoice, sum from invoices where uid = {$yaCustomerNumber} and invoice = {$yaInvoiceId} and sum = {$yaOrderSumAmount}";
 			file_put_contents('query.log', $query);
 			$result = pg_query($query);

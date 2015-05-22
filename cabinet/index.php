49f10a081cd143a631984c161cca500004ed567f
Arsen Bespalov <me@arsen.pw>
2015-05-22 21:57:12 +0800

    update cabinet

--- cabinet/index.php
+++ cabinet/index.php
@@ -809,6 +809,7 @@ function updateRubric($rubric_id, $industry_id) {
 		if ($conf['db']['type'] == 'postgres') {
 			$db = pg_connect('dbname='.$conf['db']['database']) or die('Невозможно подключиться к БД: '.pg_last_error());
 			$query = "update rubrics set industry_id = {$industry_id} where alias = (select alias from rubrics where id = {$rubric_id})";
+			pg_query($query);
 			pg_close($db);
 			echo $query;
 		}

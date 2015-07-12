518d63150f709c146eb5a20d0c510fa1fc2076cc
Arsen Bespalov <me@arsen.pw>
2015-07-12 09:24:53 +0800

    update pdf config

--- cabinet/src/vendor/tecnick.com/tcpdf/config/tcpdf_config.php
+++ cabinet/src/vendor/tecnick.com/tcpdf/config/tcpdf_config.php
@@ -63,7 +63,7 @@
  * Default images directory.
  * By default it is automatically set but you can also set it as a fixed string to improve performances.
  */
-define ('K_PATH_IMAGES', '/home/cnamrf.ru/www/images/');
+define ('K_PATH_IMAGES', '/var/www/html/images/');
 
 /**
  * Deafult image logo used be the default Header() method.
